2d23055ab9a81dc37ec444b0bebbe23946d761c3
manhtranduy <manhtranduy1993@gmail.com>
2024-05-17 14:25:06 +0200

    update OLI LUTs

--- common/SensorBands.m
+++ common/SensorBands.m
@@ -5,19 +5,21 @@ classdef SensorBands
         meris = [400 412 443 490 510 560 620 665 674 681 709 754 779];
         msi =   [        443 490     560     665         705 740     783];
         modis = [    412 443 488 531 551     667     678     748];
-        oli =   [        443 483     561     655                          865];
+        oli =   [        443 490     560     655                          865];
 
         % Typical visible bands
         olci_vis =  [412 443 490 510 560 665];
         meris_vis = [412 443 490 510 560 665];
         msi_vis =   [    443 490     560 665];
         modis_vis = [412 443 488 531 551 667];
+        oli_vis =   [443     490     560 655];
 
         % Typical visible & NIR      
         olci_vis_nir =  [412 443 490 510 560 665 709];
         meris_vis_nir = [412 443 490 510 560 665 709];
         msi_vis_nir =   [    443 490     560 665 705];
         modis_vis_nir = [412 443 488 531 551 667     748];
+        oli_vis_nir =   [443     490     560 655         865];
     end
     
 %     methods (Static)
