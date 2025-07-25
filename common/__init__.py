8009ba3b6a0e7724faa17011ae54acff47a2b637
Manh TRAN <69975017+manhtranduy@users.noreply.github.com>
2025-07-25 13:31:51 +0200

    update installation

---
+++ common/__init__.py
@@ -0,0 +1,5 @@
+# common/__init__.py
+"""Chl-CONNECT: Combination Of Neural Network models for Estimating Chlorophyll-a"""
+
+__version__ = "1.0.0"
+__doc__ = "Combination Of Neural Network models for Estimating Chlorophyll-a over Turbid and Clear Waters"
